df465d822d43411ebb7ff986cb0b4e9fcbed1fc0
mehedi <mehedi25122003@gmail.com>
2025-01-29 23:19:00 +0600

    delete for another practice

--- home.js
+++ home.js
@@ -3,10 +3,7 @@ document.getElementById('add-btn').addEventListener('click', function(event){
     const MoneyAmmount = document.getElementById('add-input').value;
     const getPin = document.getElementById('add-pin').value;
     const Balance = document.getElementById('main-balance').innerText;
-    const Balancen = parseInt(Balance);
-    const newone = Balancen + 54;
     
-    console.log(newone);
 
     console.log(MoneyAmmount, getPin);
 })
