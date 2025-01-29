b8b189c7b201cae049ae30ce44bfad491ac0654b
mehedi <mehedi25122003@gmail.com>
2025-01-29 21:17:42 +0600

    popup added:

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ document.getElementById('login-btn').addEventListener('click', function(event){
     const numberMail = document.getElementById('input-value').value;
     const passPin = document.getElementById('pass-value').value;
     event.preventDefault();
-    console.log(numberMail, passPin);
+    if(numberMail === '313' && passPin === '1234'){
+
+    }
+    else{
+        const division = document.getElementById('popUp');
+        const p = document.createElement('p');
+        p.innerText = 'Wrong! Try again..';
+        division.appendChild(p);
+    }
 })
