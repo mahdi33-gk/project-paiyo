e4528f1c6c7347852b64b7c3511a2aff17ab7ed2
mehedi <mehedi25122003@gmail.com>
2025-01-29 21:22:20 +0600

    cant add the classes

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ document.getElementById('login-btn').addEventListener('click', function(event){
     const passPin = document.getElementById('pass-value').value;
     event.preventDefault();
     if(numberMail === '313' && passPin === '1234'){
-
+        window.location.href = '/home.html'
     }
     else{
         const division = document.getElementById('popUp');
         const p = document.createElement('p');
         p.innerText = 'Wrong! Try again..';
+        p.classList.add = 'text-[1px]';
         division.appendChild(p);
     }
 })
