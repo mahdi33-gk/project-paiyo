3af69b34fc85c2e44bf4d1ce2730748ea4b55594
mehedi <mehedi25122003@gmail.com>
2025-01-29 21:29:49 +0600

    againn fail

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ document.getElementById('login-btn').addEventListener('click', function(event){
         const division = document.getElementById('popUp');
         const p = document.createElement('p');
         p.innerText = 'Wrong! Try again..';
-        p.classList.add = 'text-[1px]';
+        p.classList.add('text-[10px]');
         division.appendChild(p);
+        division.classList.add('flex', 'justify-center');
     }
 })
