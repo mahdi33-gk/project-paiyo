a7aae7bc2869f9d9c89c1f7f13a5be05fb339518
mehedi <mehedi25122003@gmail.com>
2025-01-29 23:17:45 +0600

    dynamic

--- home.js
+++ home.js
@@ -2,5 +2,11 @@ document.getElementById('add-btn').addEventListener('click', function(event){
     event.preventDefault();
     const MoneyAmmount = document.getElementById('add-input').value;
     const getPin = document.getElementById('add-pin').value;
+    const Balance = document.getElementById('main-balance').innerText;
+    const Balancen = parseInt(Balance);
+    const newone = Balancen + 54;
+    
+    console.log(newone);
+
     console.log(MoneyAmmount, getPin);
 })
