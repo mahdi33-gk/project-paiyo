63b1427cfdf3e3473bcb2f8775cd7d24bfb1cdfb
mehedi <mehedi25122003@gmail.com>
2025-01-29 23:09:31 +0600

    added btn and mmoney section

--- home.js
+++ home.js
@@ -1,1 +1,6 @@
-console.log('it works');
+document.getElementById('add-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const MoneyAmmount = document.getElementById('add-input').value;
+    const getPin = document.getElementById('add-pin').value;
+    console.log(MoneyAmmount, getPin);
+})
