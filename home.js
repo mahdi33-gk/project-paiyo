5397d97ecfe108c65ace6b240327111a6a28f759
mehedi <mehedi25122003@gmail.com>
2025-01-30 12:36:42 +0600

    fuck odd

--- home.js
+++ home.js
@@ -1,9 +1,14 @@
-document.getElementById('add-btn').addEventListener('click', function(event){
-    event.preventDefault();
-    const MoneyAmmount = document.getElementById('add-input').value;
-    const getPin = document.getElementById('add-pin').value;
-    const Balance = document.getElementById('main-balance').innerText;
-    
+document.getElementById("add-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+  const MoneyAmmount = document.getElementById("add-input").value;
+  const getPin = document.getElementById("add-pin").value;
+  const Balance = document.getElementById("main-balance").innerText;
+  if (getPin === "1234") {
+    const newBalance = parseInt(Balance);
+    const setInput = newBalance + MoneyAmmount;
+    Balance.innerText = setInput;
+    console.log(newBalance);
+  }
 
-    console.log(MoneyAmmount, getPin);
-})
+  console.log(MoneyAmmount, getPin);
+});
