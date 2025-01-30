1023d0a2b7cdee0e8388daff0c83c3a6ed6478d0
mehedi <mehedi25122003@gmail.com>
2025-01-30 12:59:19 +0600

    getting weird but i still dont get it

--- home.js
+++ home.js
@@ -7,7 +7,8 @@ document.getElementById("add-btn").addEventListener("click", function (event) {
   if (getPin === "1234") {
     const newBalance = parseInt(Balance);
     const showBalance = newBalance + realMoney;
-    Balance.innerText = showBalance;
+    
+    showBalance  =  Balance.innerText;
     console.log(showBalance);
     
   }
